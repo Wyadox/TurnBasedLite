74d2fc8e6fe2de3b3f3a166c30290e08877da694
Wyadox <j512carreras@gmail.com>
2025-11-29 15:58:23 -0500

    Setup Phase Update
    Added the ability to spawn in the Archer Knight, Archbishop, and Mitosis Pawn
    
    Also added restriction so that the player cannot spawn a duplicate piece

--- chess-game/scripts/setup_phase_ui.gd
+++ chess-game/scripts/setup_phase_ui.gd
@@ -1,21 +1,54 @@
 extends Control
 signal spawn_piece(piece_type)
 
+var status = Globals.COLORS.WHITE
+var white_dict = {}
+var black_dict = {}
+
+func valid_spawn(piece_type : Globals.PIECE_TYPES) -> bool:
+	if (status == Globals.COLORS.WHITE and !white_dict.has(piece_type)) or (status == Globals.COLORS.BLACK and !black_dict.has(piece_type)):
+		if (status == Globals.COLORS.WHITE):
+			white_dict[piece_type] = true
+		else:
+			black_dict[piece_type] = true
+		return true
+	return false
+	
+func _on_board_set_status(color: Variant) -> void:
+	status = color
+
 func _on_pawn_button_pressed() -> void:
 	print("Pawn")
-	emit_signal("spawn_piece", Globals.PIECE_TYPES.PAWN)
-
+	if (valid_spawn(Globals.PIECE_TYPES.PAWN)):
+		emit_signal("spawn_piece", Globals.PIECE_TYPES.PAWN)
 
 func _on_bishop_button_pressed() -> void:
 	print("Bishop")
-	emit_signal("spawn_piece", Globals.PIECE_TYPES.BISHOP)
-
+	if (valid_spawn(Globals.PIECE_TYPES.BISHOP)):
+		emit_signal("spawn_piece", Globals.PIECE_TYPES.BISHOP)
 
 func _on_knight_button_pressed() -> void:
-	print("Kinght")
-	emit_signal("spawn_piece", Globals.PIECE_TYPES.KNIGHT)
-
+	print("Knight")
+	if (valid_spawn(Globals.PIECE_TYPES.KNIGHT)):
+		emit_signal("spawn_piece", Globals.PIECE_TYPES.KNIGHT)
 
 func _on_king_button_pressed() -> void:
 	print("King")
-	emit_signal("spawn_piece", Globals.PIECE_TYPES.KING)
+	if (valid_spawn(Globals.PIECE_TYPES.KING)):
+		emit_signal("spawn_piece", Globals.PIECE_TYPES.KING)
+
+func _on_archer_button_pressed() -> void:
+	print("Archer")
+	if (valid_spawn(Globals.PIECE_TYPES.HORSE_ARCHER)):
+		emit_signal("spawn_piece", Globals.PIECE_TYPES.HORSE_ARCHER)
+
+func _on_archbishop_button_pressed() -> void:
+	print("Archbishop")
+	if (valid_spawn(Globals.PIECE_TYPES.ARCHBISHOP)):
+		emit_signal("spawn_piece", Globals.PIECE_TYPES.ARCHBISHOP)
+
+
+func _on_mitosis_button_pressed() -> void:
+	print("Mitosis")
+	if (valid_spawn(Globals.PIECE_TYPES.MITOSIS_PAWN)):
+		emit_signal("spawn_piece", Globals.PIECE_TYPES.MITOSIS_PAWN)
