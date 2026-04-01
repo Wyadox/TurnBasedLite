577211cd790a6a9a9ebd833dff88c56f710cc178
BoldFold <ryanbeatzu@gmail.com>
2026-04-01 15:29:19 -0400

    Revert "Merge branch 'ryan'"
    This reverts commit 6548a7d5db86a35e3303c6b1bb070a8e334c465b, reversing
    changes made to 2753d39085a72423106ff4434119d20b4564ae73.

--- chess-game/scripts/options_menu.gd
+++ chess-game/scripts/options_menu.gd
@@ -6,47 +6,37 @@ extends Control
 
 var current_map : int = -1
 var color : Globals.COLORS
-var game_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	const GAME_SCENE = preload("res://scenes/game.tscn")
-	game_scene = GAME_SCENE.instantiate()
+	pass # Replace with function body.
 	
 func _on_continue_button_pressed() -> void:
 	if opponent_option_button.get_selected_id() == -1 or difficulty_option_button.get_selected_id() == -1 or current_map == -1:
 		notification_node.set_text("An option was not selected")
 		return
 	
-	#print("options menu")
-	
+	const GAME_SCENE = preload("res://scenes/game.tscn")
+	var game_scene = GAME_SCENE.instantiate()
 	
 	game_scene.player2_type = convert_opponent_option(opponent_option_button.get_item_text(opponent_option_button.get_selected_id()))
 	game_scene.difficulty = difficulty_option_button.get_item_text(difficulty_option_button.get_selected_id())
 	game_scene.current_map = current_map
-	#print("game scene current map is")
-	#print(game_scene.current_map)
-	
-	
 	
 	var scene_tree = get_tree()
 	scene_tree.current_scene.queue_free()
 	if game_scene.player2_type == Globals.PLAYER_2_TYPE.HUMAN:
 		scene_tree.root.add_child(game_scene)
 		scene_tree.current_scene = game_scene
-		SignalBus.emit_signal("change_map",current_map)
 	else:
 		const COLOR_SCENE = preload("res://scenes/color_menu.tscn")
 		var color_scene = COLOR_SCENE.instantiate()
-		color_scene.current_map = current_map
 		
 		color_scene.load_scene = game_scene
-		
 		
 		scene_tree.current_scene.queue_free()
 		scene_tree.root.add_child(color_scene)
 		scene_tree.current_scene = color_scene
-		
 	
 func convert_opponent_option(option : String) -> Globals.PLAYER_2_TYPE:
 	if option == "HUMAN":
@@ -56,19 +46,12 @@ func convert_opponent_option(option : String) -> Globals.PLAYER_2_TYPE:
 
 func _on_map_1_button_pressed() -> void:
 	current_map = 1
-	print(current_map)
-
 
 func _on_map_2_button_pressed() -> void:
 	current_map = 2
-	print(current_map)
-
 
 func _on_map_3_button_pressed() -> void:
 	current_map = 3
-	print(current_map)
-
 
 func _on_map_4_button_pressed() -> void:
 	current_map = 4
-	print(current_map)
