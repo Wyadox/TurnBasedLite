7df0475bc556cd1e9164e18c3e1cef786a402868
BoldFold <ryanbeatzu@gmail.com>
2026-04-18 15:13:56 -0400

    pre sumo and wiz

--- chess-game/scripts/loadout_save_script.gd
+++ chess-game/scripts/loadout_save_script.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const SAVE_LOCATION = "user://AILoadoutsForestHard.json"
+const SAVE_LOCATION = "user://Loadouts.json"
 
 var loadouts_to_save : Dictionary = {
 	"loadout1" : "4:0.0,5.0_",
