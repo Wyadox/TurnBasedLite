0bac6cbed763cbcc2040eda4afa5734a2f28cd9c
Wyadox <j512carreras@gmail.com>
2026-04-23 14:04:13 -0400

    Juggernaut Sprite Fix

--- chess-game/scripts/juggernaut.gd
+++ chess-game/scripts/juggernaut.gd
@@ -3,9 +3,11 @@ extends Node
 func JuggernautUpdate(board, piece):
 	if piece.piece_type == Globals.PIECE_TYPES.JUGGERNAUT:
 		piece.piece_type = Globals.PIECE_TYPES.JUGGERNAUT2
+		piece.trojan_cloak_type = piece.piece_type
 		piece.update_sprite()
 		print(piece.piece_type)
 	elif piece.piece_type == Globals.PIECE_TYPES.JUGGERNAUT2:
 		piece.piece_type = Globals.PIECE_TYPES.JUGGERNAUT1
+		piece.trojan_cloak_type = piece.piece_type
 		piece.update_sprite()
 		print(piece.piece_type)
