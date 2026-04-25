8c9c506558ed9e6ca7e2601b9442cfded0266cd2
Wyadox <j512carreras@gmail.com>
2026-04-24 20:32:53 -0400

    Duplicator Sprite Update Fix

--- chess-game/scripts/duplicator.gd
+++ chess-game/scripts/duplicator.gd
@@ -2,6 +2,7 @@ extends Node
 
 func Duplicate(selected_piece, dest_piece, board):
 	selected_piece.piece_type = dest_piece.piece_type
+	selected_piece.trojan_cloak_type = dest_piece.trojan_cloak_type
 	selected_piece.update_sprite()
 	if dest_piece.piece_type == Globals.PIECE_TYPES.SHIELD_KING:
 		board.shield_king.append(selected_piece)
